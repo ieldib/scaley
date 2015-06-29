c8f3b11482b419d5e3caa7f6203df110f9b2e326
Islam Eldib <ieldib@PC101960.local>
2015-06-29 15:41:09 -0400

    First commit - some basic skeleton routes provides

---
+++ scaley.py
@@ -0,0 +1,36 @@
+#app.py
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.roue('/home')
+def home():
+    return 'Welcome to Scaley !'
+
+@app.route('/install')
+def install():
+    open('config.cfg', 'w+')
+
+
+@app.route('/config')
+def config():
+    #.... config stuff
+    #aws credentials e.t.c.
+    pass
+
+@app.route('/stats')
+def stats():
+    # stats stuff
+    # display cloudwatch metrics
+    #display openstack metrics e.t.c.
+    pass
+
+@app.route('/migrate')
+def migrate():
+    # migrate stuff here
+    pass
+
+
+if __name__ == '__main__':
+    app.run()
