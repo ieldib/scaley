5b38dc79f0b324867b0f77440ac8adac9f73516d
Islam Eldib <islam.eldib@gmail.com>
2015-07-02 11:15:50 -0400

    Redid config.py now using sqlite to retrieve/update/read config

--- module/config.py
+++ module/config.py
@@ -1,49 +1,30 @@
-import ConfigParser
 from db import *
-
-
 
 
 def default_config_write():
     with configdb_connect as connection:
         c = connection.cursor()
-        c.execute('''SQL STATEMENTS''')
+        defaultcreds = [('AWSACCESSKYEID', 'NONE')
+                        ('AWSACCESSSECRET', 'NONE')
+                        ('RACKSPACEACCESSID', 'NONE')
+                        ('RACKSPACESECRET', 'NONE')
+                        ('OPENSTACKTOKENID', 'NONE')
+                        ('OPENSTACKTOKEN', 'NONE')
+                        ('AZUREACCESSID', 'NONE')
+                        ('AZURESECRET', 'NONE')
+                       ]
+        c.executemany("INSERT INTO CLOUDCREDS VALUES (?, ?)", defaultcreds)
 
 
 
-class ConfigWrite:
+def update_config_write(value):
+    with configdb_connect as connection:
+        c = connection.cursor()
+        c.executemany("INSERT INTO CLOUDCREDS VALUES (?, ?)", value)
 
-    def write_aws_config(accesskeyid,secret):
-        #some code
-        pass
-    def write_rackspace_config(accesskeyid,secret):
-        #some code
-        pass
-    def write_openstack_config(accesskeyid,secret):
-        #somecode
-        pass
-    def write_azure_config(accesskeyid,secret):
-        #somecode
-        pass
-    def write_azure_config(accesskeyid, secret):
-        #somecode
-        pass
-
-    def default_write_config(self):
-        config['AWS'] = {'AWSACCESSKEYID': 'NONE',
-                         'AWSACESSSECRETKEY': 'NONE'}
-        config['RACKSPACE'] = {'RACKSPACEACCESSKEY': 'NONE',
-                               'RACKSPACESECRET': 'NONE'}
-        with open('../config.cfg', 'w') as configfile:
-            configfile.write(configfile)
-
-    def __init__(self):
-        self.accesskeyid = accesskeyid
-        self.secret = secret
-
-class ConfigRead:
-    def readconfig(self):
-        configread = config.read('../config.cfg')
-
-    def __init__(self):
-        self.configread = self.readconfig(configread)
+def config_read(value):
+    with configdb_connect as connection:
+        c = connection.cursor()
+        c.execute("SELECT ALL FROM CLOUDCREDS")
+        rows = c.fetchall()
+        return rows
