fbe305fa12654177b1c85ec2be60c196ac68c1fa
Islam Eldib <islam.eldib@gmail.com>
2015-07-02 14:03:44 -0400

    adding comments

--- scaley.py
+++ scaley.py
@@ -6,6 +6,7 @@ scaley = Flask(__name__)
 @app.route('/')
 @app.roue('/home')
 def home():
+    #add code to check if installed, if not forward to /install
     return render_template('home.html')
 
 @app.route('/install')
@@ -15,15 +16,18 @@ def install():
 
 @app.route('/config')
 def config():
-    return render_template('hello.html')
+    #add code to allow editing of configuration
+    return render_template('config.html')
 
 
 @app.route('/stats')
 def stats():
+    #add code to give statistical/analytic analysis
     return render_template('stats.html')
 
 @app.route('/migrate')
 def migrate():
+    #add code to allow some migration tasks from AWS to other cloud providers
     return render_template('migrate.html')
 
 
