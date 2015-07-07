fd5ef395e436d99df9635c224b0201911be3fdb7
Islam Eldib <islam.eldib@gmail.com>
2015-07-07 16:49:57 -0400

    moved config line from runserver.py

--- scaley/runserver.py
+++ scaley/runserver.py
@@ -1,6 +1,5 @@
 from os import environ
 from scaley import app
-from scaley.module import db
 
 
 if __name__ == '__main__':
@@ -9,6 +8,5 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '8080'))
     except ValueError:
         PORT = 5555
-    app.config['DEBUG'] = True  #need to move these config lines to modules.config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db/scaley.db'
+    app.config['DEBUG'] = True  #need to move these config lines to config.cfg use Flask-ini
     app.run(HOST, PORT)
