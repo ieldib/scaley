b84aa531b7a9c0eb24bf4414d8d62fcd9fd8eeb9
Islam Eldib <islam.eldib@gmail.com>
2015-10-07 14:21:01 -0400

    fixed migration routing error

--- scaley/runserver.py
+++ scaley/runserver.py
@@ -3,7 +3,7 @@ from scaley import app
 
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
         PORT = int(environ.get('SERVER_PORT', '8080'))
     except ValueError:
